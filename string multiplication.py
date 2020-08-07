d889d7f819a9e21e81988a1b5d17983eafdb8a78
JadeSpy <58379062+JadeSpy@users.noreply.github.com>
2020-08-07 10:21:18 -0700

    Add files via upload

---
+++ string multiplication.py
@@ -0,0 +1,648 @@
+#multiplication.py
+'''
+THIS CODE USES NO MATHAMATICAL OPERATORS SUCH AS + - * /
+(EXCEPT FOR STRING AND LIST MANIPULATION)
+TO PERFORM ADDITION AND MULTIPLICATION
+
+LIMITATIONS:
+cannot do decimals
+only accepts two nums at once (easy to fix)
+
+
+
+TO TEST:
+import this file then use either of these functions
+example = multiplication(999,999)
+or
+example = addition(999,999)
+'''
+
+
+
+
+
+
+#I should've just removed the first occurence of 1, whoops.
+def manualSubtractionBy10(i):
+	if i ==10:
+		return(0)
+	if i ==11:
+		return(1)
+	if i ==12:
+		return(2)
+	if i ==13:
+		return(3)
+	if i ==14:
+		return(4)
+	if i ==15:
+		return(5)
+	if i ==16:
+		return(6)
+	if i ==17:
+		return(7)
+	if i ==18:
+		return(8)
+	if i ==19:
+		return(9)
+def manualMath(i,x):
+	#print(x)
+	#print(i)
+	if i ==0:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(1)
+		if x ==2: 
+			return(2)
+		if x ==3: 
+			return(3)
+		if x ==4: 
+			return(4)
+		if x ==5: 
+			return(5)
+		if x ==6: 
+			return(6)
+		if x ==7: 
+			return(7)
+		if x ==8: 
+			return(8)
+		if x ==9: 
+			return(9)
+		if x ==10: 
+			return(10)
+	if i ==1:
+		if x ==0: 
+			return(1)
+		if x ==1: 
+			return(2)
+		if x ==2: 
+			return(3)
+		if x ==3: 
+			return(4)
+		if x ==4: 
+			return(5)
+		if x ==5: 
+			return(6)
+		if x ==6: 
+			return(7)
+		if x ==7: 
+			return(8)
+		if x ==8: 
+			return(9)
+		if x ==9: 
+			return(10)
+		if x ==10: 
+			return(11)
+	if i ==2:
+		if x ==0: 
+			return(2)
+		if x ==1: 
+			return(3)
+		if x ==2: 
+			return(4)
+		if x ==3: 
+			return(5)
+		if x ==4: 
+			return(6)
+		if x ==5: 
+			return(7)
+		if x ==6: 
+			return(8)
+		if x ==7: 
+			return(9)
+		if x ==8: 
+			return(10)
+		if x ==9: 
+			return(11)
+		if x ==10: 
+			return(12)
+	if i ==3:
+		if x ==0: 
+			return(3)
+		if x ==1: 
+			return(4)
+		if x ==2: 
+			return(5)
+		if x ==3: 
+			return(6)
+		if x ==4: 
+			return(7)
+		if x ==5: 
+			return(8)
+		if x ==6: 
+			return(9)
+		if x ==7: 
+			return(10)
+		if x ==8: 
+			return(11)
+		if x ==9: 
+			return(12)
+		if x ==10: 
+			return(13)
+	if i ==4:
+		if x ==0: 
+			return(4)
+		if x ==1: 
+			return(5)
+		if x ==2: 
+			return(6)
+		if x ==3: 
+			return(7)
+		if x ==4: 
+			return(8)
+		if x ==5: 
+			return(9)
+		if x ==6: 
+			return(10)
+		if x ==7: 
+			return(11)
+		if x ==8: 
+			return(12)
+		if x ==9: 
+			return(13)
+		if x ==10: 
+			return(14)
+	if i ==5:
+		if x ==0: 
+			return(5)
+		if x ==1: 
+			return(6)
+		if x ==2: 
+			return(7)
+		if x ==3: 
+			return(8)
+		if x ==4: 
+			return(9)
+		if x ==5: 
+			return(10)
+		if x ==6: 
+			return(11)
+		if x ==7: 
+			return(12)
+		if x ==8: 
+			return(13)
+		if x ==9: 
+			return(14)
+		if x ==10: 
+			return(15)
+	if i ==6:
+		if x ==0: 
+			return(6)
+		if x ==1: 
+			return(7)
+		if x ==2: 
+			return(8)
+		if x ==3: 
+			return(9)
+		if x ==4: 
+			return(10)
+		if x ==5: 
+			return(11)
+		if x ==6: 
+			return(12)
+		if x ==7: 
+			return(13)
+		if x ==8: 
+			return(14)
+		if x ==9: 
+			return(15)
+		if x ==10: 
+			return(16)
+	if i ==7:
+		if x ==0: 
+			return(7)
+		if x ==1: 
+			return(8)
+		if x ==2: 
+			return(9)
+		if x ==3: 
+			return(10)
+		if x ==4: 
+			return(11)
+		if x ==5: 
+			return(12)
+		if x ==6: 
+			return(13)
+		if x ==7: 
+			return(14)
+		if x ==8: 
+			return(15)
+		if x ==9: 
+			return(16)
+		if x ==10: 
+			return(17)
+	if i ==8:
+		if x ==0: 
+			return(8)
+		if x ==1: 
+			return(9)
+		if x ==2: 
+			return(10)
+		if x ==3: 
+			return(11)
+		if x ==4: 
+			return(12)
+		if x ==5: 
+			return(13)
+		if x ==6: 
+			return(14)
+		if x ==7: 
+			return(15)
+		if x ==8: 
+			return(16)
+		if x ==9: 
+			return(17)
+		if x ==10: 
+			return(18)
+	if i ==9:
+		if x ==0: 
+			return(9)
+		if x ==1: 
+			return(10)
+		if x ==2: 
+			return(11)
+		if x ==3: 
+			return(12)
+		if x ==4: 
+			return(13)
+		if x ==5: 
+			return(14)
+		if x ==6: 
+			return(15)
+		if x ==7: 
+			return(16)
+		if x ==8: 
+			return(17)
+		if x ==9: 
+			return(18)
+		if x ==10: 
+			return(19)
+	if i ==10:
+		if x ==0: 
+			return(10)
+		if x ==1: 
+			return(11)
+		if x ==2: 
+			return(12)
+		if x ==3: 
+			return(13)
+		if x ==4: 
+			return(14)
+		if x ==5: 
+			return(15)
+		if x ==6: 
+			return(16)
+		if x ==7: 
+			return(17)
+		if x ==8: 
+			return(18)
+		if x ==9: 
+			return(19)
+		if x ==10: 
+			return(20)
+def manualMultiplication(i,x):
+	if i ==0:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(0)
+		if x ==2: 
+			return(0)
+		if x ==3: 
+			return(0)
+		if x ==4: 
+			return(0)
+		if x ==5: 
+			return(0)
+		if x ==6: 
+			return(0)
+		if x ==7: 
+			return(0)
+		if x ==8: 
+			return(0)
+		if x ==9: 
+			return(0)
+	if i ==1:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(1)
+		if x ==2: 
+			return(2)
+		if x ==3: 
+			return(3)
+		if x ==4: 
+			return(4)
+		if x ==5: 
+			return(5)
+		if x ==6: 
+			return(6)
+		if x ==7: 
+			return(7)
+		if x ==8: 
+			return(8)
+		if x ==9: 
+			return(9)
+	if i ==2:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(2)
+		if x ==2: 
+			return(4)
+		if x ==3: 
+			return(6)
+		if x ==4: 
+			return(8)
+		if x ==5: 
+			return(10)
+		if x ==6: 
+			return(12)
+		if x ==7: 
+			return(14)
+		if x ==8: 
+			return(16)
+		if x ==9: 
+			return(18)
+	if i ==3:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(3)
+		if x ==2: 
+			return(6)
+		if x ==3: 
+			return(9)
+		if x ==4: 
+			return(12)
+		if x ==5: 
+			return(15)
+		if x ==6: 
+			return(18)
+		if x ==7: 
+			return(21)
+		if x ==8: 
+			return(24)
+		if x ==9: 
+			return(27)
+	if i ==4:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(4)
+		if x ==2: 
+			return(8)
+		if x ==3: 
+			return(12)
+		if x ==4: 
+			return(16)
+		if x ==5: 
+			return(20)
+		if x ==6: 
+			return(24)
+		if x ==7: 
+			return(28)
+		if x ==8: 
+			return(32)
+		if x ==9: 
+			return(36)
+	if i ==5:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(5)
+		if x ==2: 
+			return(10)
+		if x ==3: 
+			return(15)
+		if x ==4: 
+			return(20)
+		if x ==5: 
+			return(25)
+		if x ==6: 
+			return(30)
+		if x ==7: 
+			return(35)
+		if x ==8: 
+			return(40)
+		if x ==9: 
+			return(45)
+	if i ==6:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(6)
+		if x ==2: 
+			return(12)
+		if x ==3: 
+			return(18)
+		if x ==4: 
+			return(24)
+		if x ==5: 
+			return(30)
+		if x ==6: 
+			return(36)
+		if x ==7: 
+			return(42)
+		if x ==8: 
+			return(48)
+		if x ==9: 
+			return(54)
+	if i ==7:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(7)
+		if x ==2: 
+			return(14)
+		if x ==3: 
+			return(21)
+		if x ==4: 
+			return(28)
+		if x ==5: 
+			return(35)
+		if x ==6: 
+			return(42)
+		if x ==7: 
+			return(49)
+		if x ==8: 
+			return(56)
+		if x ==9: 
+			return(63)
+	if i ==8:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(8)
+		if x ==2: 
+			return(16)
+		if x ==3: 
+			return(24)
+		if x ==4: 
+			return(32)
+		if x ==5: 
+			return(40)
+		if x ==6: 
+			return(48)
+		if x ==7: 
+			return(56)
+		if x ==8: 
+			return(64)
+		if x ==9: 
+			return(72)
+	if i ==9:
+		if x ==0: 
+			return(0)
+		if x ==1: 
+			return(9)
+		if x ==2: 
+			return(18)
+		if x ==3: 
+			return(27)
+		if x ==4: 
+			return(36)
+		if x ==5: 
+			return(45)
+		if x ==6: 
+			return(54)
+		if x ==7: 
+			return(63)
+		if x ==8: 
+			return(72)
+		if x ==9: 
+			return(81)
+#print(manualMath(9,9))
+
+
+
+#MANUAL FUNCTION CREATOR
+'''
+out1 = "def manualMultiplication(i,x):"
+fileValue = open('manualSubtractionBy10.py', "w")
+for i in range (0,10):
+	out1 += "\n\tif i ==" + str(i) +":"
+	for x in range (0, 10):
+		out1 += "\n\t\tif x ==" + str(x) + ": \n\t\t\treturn("+str(x*i)+")"
+
+fileValue.write(out1)
+exit()
+
+'''
+'''
+out1 = "def manualMath(i,x):"
+fileValue = open('manualSubtractionBy10.py', "w")
+for i in range (0,11):
+	out1 += "\n\tif i ==" + str(i) +":"
+	for x in range (0, 11):
+		out1 += "\n\t\tif x ==" + str(x) + ": \n\t\t\treturn(" +str(x+i) +")"
+
+fileValue.write(out1)
+exit()
+'''
+'''
+out1 = "def manualSubtractionBy10(i):"
+fileValue = open('manualSubtractionBy10.py', "w")
+for i in range (10,20):
+	out1 += "\n\tif i ==" + str(i) +":"
+	out1 += "\n\t\treturn("+ str(i-10) +")"
+fileValue.write(out1)
+exit()
+'''
+
+
+def reverseList(ListInputted):
+	NewList = []
+	sofl = len(ListInputted)
+	#print(sofl)
+	for i in range (0, sofl):
+		NewList = NewList + list(str(ListInputted[(sofl-i)-1]))
+
+		#ALTERNATE METHOD
+		#NewList = list(str(ListInputted[i])) + NewList
+	return(NewList)
+
+def addition(num1, num2):
+	num1= list(str(num1))
+	num2= list(str(num2))
+	c = len(num1)
+	d = len(num2)
+	num2 = reverseList(num2)
+	num1 = reverseList(num1)
+	if (c<d):
+		numTemp = num2
+		num2 = num1
+		num1 = numTemp
+	if (c != d):
+		iterations = ""
+		for i in num2:
+			iterations2 = len(iterations)
+			num1[iterations2] = int(str((num1[iterations2])).strip())
+			x = 0
+			x =  manualMath(int(i),int(num1[iterations2])) # 
+			testL = len(list(str(x)))
+			if testL > 1:
+				x = manualSubtractionBy10(x)
+				num1[iterations2+1] = int(num1[iterations2+1]) + 1
+			num1[iterations2] = x
+			iterations = iterations + "I"
+		num1 = reverseList(num1)
+		finalNum = ""
+		for y in num1:
+			finalNum += y
+		return(finalNum)
+	else:
+		iterations = ""
+		for i in num2:
+			iterations2 = len(iterations)
+			num1[iterations2] = int(str((num1[iterations2])).strip())
+			x = 0
+			x =  manualMath(int(i),int(num1[iterations2])) #   
+			testL = len(list(str(x)))
+			if testL > 1:
+				x = manualSubtractionBy10(x)
+				try:
+					num1[iterations2+1] = int(num1[iterations2+1]) + 1
+				except:
+					num1.append(0)
+					num1[iterations2+1] = int(num1[iterations2+1]) + 1
+			num1[iterations2] = x
+			iterations = iterations + "I"
+		num1 = reverseList(num1)
+		finalNum = ""
+		for y in num1:
+			finalNum += y
+		return(finalNum)
+def multiplication(num1,num2):
+	total = 0
+	zerosadded1=""
+	zerosadded2=""
+	num1= list(str(num1))
+	num2= list(str(num2))
+	###REPLACE THIS WITH OG CODE
+	num2 = reverseList(num2)
+	num1 = reverseList(num1)
+	for a in num1:
+		zerosadded2=""
+		for b in num2:
+			out = manualMultiplication(int(a),int(b))
+			out = (str(out) + zerosadded1 + zerosadded2)
+			out = int(out)
+			total = out + total
+
+			zerosadded2 = zerosadded2 + "0"
+
+
+
+		zerosadded1= zerosadded1 + "0"
+	return(total)
+
+#print(addition(34,999))
+print(multiplication(7,8))
+example = addition(54,52)
+print(example)
+
+
+
+
+
+
